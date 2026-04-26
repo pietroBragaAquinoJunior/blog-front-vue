985a0eeb468c25eaabfca6a6899695940c014cce
pietro <pietrobragaprofissional@gmail.com>
2026-04-26 13:29:39 -0300

    usando tiptap

--- src/views/CreatePostView.vue
+++ src/views/CreatePostView.vue
@@ -1,16 +1,21 @@
 <script setup lang="ts">
 import type { AxiosError } from 'axios'
 import http from '../axios/http'
-import { ref } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import type { ApiError } from '@/interfaces/ApiError'
 import type { SuccessResponse } from '@/interfaces/SuccessResponse'
-import { QuillEditor } from '@vueup/vue-quill'
-import '@vueup/vue-quill/dist/vue-quill.snow.css';
+
+import Document from '@tiptap/extension-document'
+import Image from '@tiptap/extension-image'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import { Dropcursor } from '@tiptap/extensions'
+import { Editor, EditorContent } from '@tiptap/vue-3'
 
 const title = ref('')
 const description = ref('')
 const published = ref(false)
-const quill = ref(null);
+// const quill = ref(null);
 
 const loading = ref(false)
 const error = ref(null)
@@ -18,6 +23,7 @@ const error = ref(null)
 const resetForm = () => {
   title.value = ''
   description.value = ''
+  editor.value?.commands.clearContent()
   published.value = false
 }
 
@@ -26,12 +32,11 @@ const createPost = () => {
   error.value = null
 
   http.post('/posts', {
-      title: title.value,
-      description: description.value,
-      // @ts-expect-error A ref quill não está tipada, só por isso ele está reclamando.
-      html: quill.value.getHTML(),
-      published: published.value,
-    })
+    title: title.value,
+    description: description.value,
+    html: editor.value?.getHTML(),
+    published: published.value,
+  })
     .then(function (response) {
       resetForm()
       const responseTyped: SuccessResponse = response.data as SuccessResponse
@@ -47,6 +52,28 @@ const createPost = () => {
       loading.value = false
     })
 }
+
+//https://tiptap.dev/docs/editor/getting-started/install/vue3
+//https://tiptap.dev/docs/editor/extensions/nodes/image
+const imageConfiguration = Image.configure({
+  allowBase64: true,
+  resize: {
+    enabled: true,
+    alwaysPreserveAspectRatio: true
+  }
+})
+const editor = ref<Editor | undefined>();
+
+onMounted(() => {
+  editor.value = new Editor({
+    extensions: [Document, Paragraph, Text, imageConfiguration, Dropcursor],
+    content: ``,
+  })
+})
+
+onBeforeUnmount(() => {
+  editor.value?.destroy();
+})
 </script>
 
 <template>
@@ -56,7 +83,7 @@ const createPost = () => {
         <p class="form-title">Criar Postagem</p>
         <p>Título: <input type="text" required v-model="title" /></p>
         <p>Descrição: <input type="text" required v-model="description" /></p>
-        <QuillEditor ref="quill" style="min-height: 40vh; font-size: 18px;" toolbar="full" theme="snow" />
+        <p>Conteúdo: <editor-content style="border: 1px solid black;" :editor="editor" /></p>
         <p>Publicar: <input type="checkbox" v-model="published" /></p>
         <button type="submit">Enviar</button>
       </form>
